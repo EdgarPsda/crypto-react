4c82428461a571011ec1773df2f56af27f7a25ff
EdgarPsda <edgar.psda@gmail.com>
2024-01-24 14:19:40 -0800

    Crypto API call
    Added crypto API call and show list in select field.

---
+++ src/components/Form.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react"
+import styled from "@emotion/styled"
+import useSelectCurrency from "../hooks/useSelectCurrency"
+import { currencies } from "../data/currency"
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+    margin-top: 30px;
+
+    &:hover{
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+`
+
+const Form = () => {
+
+    const [cryptos, setCryptos] = useState([]);
+    const [currency, SelectCurrency] = useSelectCurrency('Select Currency', currencies);
+    const [cryptoCurrency, SelectCryptoCurrency] = useSelectCurrency('Select Cryptocurrency', cryptos);
+
+    useEffect(() => {
+        const apiRequest = async () => {
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+            const res = await fetch(url);
+            const result = await res.json();
+
+            const cryptoArray = result.Data.map(crypto => {
+                const obj = {
+                    id: crypto.CoinInfo.Name,
+                    name: crypto.CoinInfo.FullName
+                }
+
+                return obj;
+            });
+
+            setCryptos(cryptoArray);
+
+        }
+
+        apiRequest();
+    }, [])
+
+    return (
+        <form>
+            <SelectCurrency></SelectCurrency>
+            <SelectCryptoCurrency></SelectCryptoCurrency>
+            <InputSubmit type="submit" value="Quote" />
+        </form>
+    )
+}
+
+export default Form
---
+++ src/data/currency.jsx
@@ -0,0 +1,10 @@
+const currencies = [
+    { id: 'USD', name: 'USA dollar' },
+    { id: 'MXN', name: 'Mexican peso' },
+    { id: 'EUR', name: 'Euro' },
+    { id: 'GBP', name: 'Pund Sterling' },
+];
+
+export {
+    currencies
+}
